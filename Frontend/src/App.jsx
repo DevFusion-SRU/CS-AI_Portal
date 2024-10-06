403249d7403421b938a340e33ec53fb35067a438
sairamarapu <146324707+sairamarapu@users.noreply.github.com>
2024-10-06 10:50:06 +0530

    Added Directories to work on (#10)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Dashboard from './Pages/Dashboard'
+import Launchpad from './Pages/Launchpad'
+import Reports from './Pages/Reports'
+import Settings from './Pages/Settings'
+import Profile from './Pages/Profile'
+import Sidebar from './Component/sidebar'
+
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Sidebar />
+    </div>
   )
 }
+
+export default App
---
+++ Frontend/src/Pages/Launchpad.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Launchpad = () => {
+  return (
+    <div>
+      Launchpad
+    </div>
+  )
+}
+
+export default Launchpad
