8f12157c3092acab9b4bcbe39e17d6df376a84f7
sairamarapu <146324707+sairamarapu@users.noreply.github.com>
2024-10-23 12:01:08 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard'
 import Launchpad from './Pages/Launchpad'
 import Reports from './Pages/Reports'
 import Settings from './Pages/Settings'
 import Profile from './Pages/Profile'
-import Sidebar from './Component/sidebar'
-
-
+import Sidebar from './Component/Sidebar'
+import Navbar from './Component/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex- md:flex-row">
+      {/* Sidebar */}
       <Sidebar />
-      <div className="flex-grow p-6"> {/* This div will take the remaining space */}
+      
+      {/* Content Area */}
+      <div className="flex-1 md:ml-48 mt-20 md:mt-0">{/* Add margin on large screens */}
+        <div className='hidden md:block'>
+          <Navbar />
+        </div>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/launchpad" element={<Launchpad />} />
           <Route path="/myreports" element={<Reports />} />
-          <Route path="/myaccount" element={<Profile />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="/myaccount" element={<Profile />} />
         </Routes>
       </div>
     </div>
