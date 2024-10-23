7767b76abfa279bc1d73829c7ec5fcf78ab07bea
sairamarapu <146324707+sairamarapu@users.noreply.github.com>
2024-10-23 12:06:53 +0530

    Update Navbar.jsx

--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
 
+const Navbar = () => {
+  return (
+    <div className=' flex flex-row-reverse w-full h-12 bg-white shadow-md border-b-2  border-gray-200'>
+        navbar
+    </div>
+  )
+}
