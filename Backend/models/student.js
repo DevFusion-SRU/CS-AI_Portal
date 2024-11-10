91e17d469e1a33a80c444a2f84bdb0e79b6274ef
manichandra-donthula <mrmanichandra222@gmail.com>
2024-11-10 23:54:03 +0530

    Added student model, and some changes are pending

---
+++ Backend/models/student.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { studentConn } from "../config/db.js";
+
+const studentSchema = new mongoose.Schema({
+    rollNumber: { type: String, required: true },
+    name: { type: String, required: true },
+    course: { type: String, required: false },
+    email: { type: String, required: true },
+    mobile: { type: Number, required: false },
+    // timestamps: true //createdAt, updatedAt
+});
+
+const Student = studentConn.model("Student", studentSchema);
+
+export default Student;
