ca497c5338a60013afc5e3f80667c5a2e90beae6
manichandra-donthula <mrmanichandra222@gmail.com>
2024-11-19 23:58:24 +0530

    Added profile photo upload

--- Backend/routes/student.js
+++ Backend/routes/student.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { addStudent, addStudentBatch, getStudentDetails, getStudents } from "../controllers/student.js";
+import multer from "multer";
+import { addStudent, addStudentBatch, getStudentDetails, getStudents, uploadStudentPhoto } from "../controllers/student.js";
 
 const router = express.Router();
+
+// Multer configuration
+const storage = multer.memoryStorage(); // Store file in memory as a Buffer
+const upload = multer({ storage });
 
 router.get("/", getStudents);
 router.get("/:rollNumber", getStudentDetails),
 router.post("/", addStudent);
 router.post("/batch", addStudentBatch);
+router.patch("/:rollNumber/photo", upload.single("photo"), uploadStudentPhoto);
 
 export default router;
