1001830376a45b4a7cfb1d5aee806c090f542903
manichandra-donthula <mrmanichandra222@gmail.com>
2024-11-21 13:44:14 +0530

    Added views and applications route and function

--- Backend/routes/appliedJobs.js
+++ Backend/routes/appliedJobs.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { addApplication, addView, getApplications } from "../controllers/jobApplications.js";
+import { addApplication, addView, getApplications, getAppliedStudents } from "../controllers/jobApplications.js";
 
 const router = express.Router();
 
 router.post("/", addApplication);
 router.post("/view", addView);
+router.get("/allDetails", getAppliedStudents);
 router.get("/:rollNumber", getApplications);
 
 export default router;
