7ac0170ad5777cfe0d88edeac0d004de518c2e3d
manichandra-donthula <mrmanichandra222@gmail.com>
2024-11-23 22:34:22 +0530

    Added search functionalities for testing

--- Backend/routes/search.js
+++ Backend/routes/search.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 import { authenticateToken, authorizeRole } from "../middleware/auth.js";
-import { searchJobs } from "../controllers/search.js";
+import { searchAllDetails, searchJobs, searchStudents } from "../controllers/search.js";
 
 const router = express.Router();
 
+// Public route for now
 router.post("/jobs", searchJobs);
+
+router.post("/students", authenticateToken, authorizeRole("admin"), searchStudents);
+router.post("/allDetails", authenticateToken, authorizeRole("admin"), searchAllDetails);
 
 export default router;
