c2ae76284607152e6d591363e98d840cc81fb301
manichandra-donthula <mrmanichandra222@gmail.com>
2025-01-10 20:31:45 +0530

    Modified password changing

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
     login,
     logout,
-    forgotPassword,
     resetPassword,
     changePassword,
     verifyTokenController
@@ -14,7 +13,6 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/logout", authenticateToken, logout); // Use `authenticateToken` for logout (optional)
 
-router.post("/forgot-password", forgotPassword); // Forgot password route (sends OTP to email)
 router.post("/reset-password", resetPassword); // Reset password route (validates OTP and resets password)
 router.put("/change-password", authenticateToken, authorizeRole(["admin", "student"]), changePassword); // Change password route (validates current, new passwords and changes)
 
